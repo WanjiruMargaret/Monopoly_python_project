50094e02a0359461db01dfed5ec3ee337d64defc
Euphraith <maganjoeuphraith256@gmail.com>
2025-08-21 22:52:47 +0300

    adding monopoly file

---
+++ board.py
@@ -0,0 +1,14 @@
+## board and tile setup
+
+board = [ ## id borad that has a row that is loop into
+    "Go", 
+    "property 1",
+    "chance", 
+    "property 2", 
+    "jail",## tiles
+    "property 3", 
+    "free parking", 
+    "property 4", 
+    "Go to jail", 
+    "property 5"
+]
