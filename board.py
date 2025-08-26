b45abfc9311c0318a60337d9c6f9a2e4ce008089
Euphraith <maganjoeuphraith256@gmail.com>
2025-08-26 11:34:27 +0300

    initial commit

--- board.py
+++ board.py
@@ -12,3 +12,11 @@ board = [ ## id borad that has a row that is loop into
     "Go to jail", 
     "property 5"
 ]
+
+def real_board(board):
+    print("Board and tiles")
+    for p,in range(board): ## looping throug
+        print(f"{i}: {board[p]}")
+
+real_board(board) ## calling out the board 
+
